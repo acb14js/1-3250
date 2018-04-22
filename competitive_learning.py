90d393aaa3089603e16bdd9b615bd6cdb2bc28fd
acb14js <jsawtellmason1@sheffield.ac.uk>
2018-04-22 17:03:00 +0000

    Changes to plotting

--- competitive_learning.py
+++ competitive_learning.py
@@ -3,15 +3,14 @@ import numpy.matlib
 import math
 import matplotlib.pyplot as plt
 
-def mainloop(leaky=False, dinit=False, neighbours=False):
+def mainloop(leaky=False, dinit=False, neighbours=False, show=False):
     train = np.genfromtxt('digits/train.csv', delimiter=",")
     trainlabels = np.genfromtxt('digits/trainlabels.csv', delimiter=",")
     
     # number of pixels and number of training data
     [n, m] = np.shape(train) # 784 * 5000, features * samples
     eta = 5E-2 # Learning rate of the wining unit
-    theta = 5E-5 # Learning rate of other units. An order of magnitude smaller.
-    # parameter controlling magnitude of initial conditions
+    theta = 5E-5 # Learning rate of other units. An few orders of magnitude smaller.
     winit = 1 # This scales the random numbers
     alpha = 0.999 # This is just a graphing term.
     cost = 0
@@ -19,6 +18,7 @@ def mainloop(leaky=False, dinit=False, neighbours=False):
     tmax = 40000 # The amount of timesteps/iterations of the algorithm.
     digits = 15 # the amount of class reconstructions. 
 
+    # Initialising from a random sample in the data
     if dinit:
         index = np.random.choice(n, digits)
         winit = train[index]
@@ -28,7 +28,7 @@ def mainloop(leaky=False, dinit=False, neighbours=False):
     W = winit * np.random.rand(digits, n) # Random weights for the network
     normW = np.sqrt(np.diag(W.dot(W.T)))
     # reshape normW into a numpy 2d array
-    normW = normW.reshape(digits, -1) # -1 means that the dimension is inferred.
+    normW = normW.reshape(digits, -1)
 
     #W = W / np.matlib.repmat(normW.T,n,1).T    # normalise using repmat
     # normalise using numpy broadcasting -  http://docs.scipy.org/doc/numpy-1.10.1/user/basics.broadcasting.html
@@ -36,30 +36,27 @@ def mainloop(leaky=False, dinit=False, neighbours=False):
     # 15 * 784, d*n
 
     counter = np.zeros((1, digits))              # counter for the number of times the neuron wins
-    # counter = np.zeros((1, digits))
+    dead_units = None                            # Init the dead units list
 
-    dead_units = None # Init the dead units list
     # running avg of the weight change over time
     wCount = np.ones((1, tmax+1)) * 0.25  # init the weight change over time
 
-    # NBVAL_SKIP
-    plt.ion()                                   # interactive mode on
-    plt.close('all')
+    if show:
+        # NBVAL_SKIP
+        plt.ion()                                   # interactive mode on
+        plt.close('all')
 
-    # counter for the rows of the subplot
-    yl = int(round(digits/5)) 
-    if digits % 5 != 0:
-        yl += 1
+        # counter for the rows of the subplot
+        yl = int(round(digits/5)) 
+        if digits % 5 != 0:
+            yl += 1
 
-    fig_neurs, axes_neurs = plt.subplots(
-        yl, 5, figsize=(5, 4))  # fig for the output neurons
-    fig_stats, axes_stats = plt.subplots(
-        5, 1, figsize=(5, 7))   # fig for the learning stats
+        fig_neurs, axes_neurs = plt.subplots(
+            yl, 5, figsize=(5, 4))  # fig for the output neurons
+        fig_stats, axes_stats = plt.subplots(
+            5, 1, figsize=(5, 7))   # fig for the learning stats
         
     for t in range(1, tmax): 
-        
-        baseline = np.mean(counter)
-
         # get a randomly generated index in the input range
         i = math.ceil(m * np.random.rand())-1 
         # pick a training instance using the random index
@@ -109,65 +106,70 @@ def mainloop(leaky=False, dinit=False, neighbours=False):
 
         cost += 0.5*(W[k, :] - x)**2
 
-        if not t % 300 or t == 1:
-            #   for ii in range(yl):
-            #       for jj in range(5):
-            #           if 5*ii+jj < digits:
-            #               prototypes = W[5*ii+jj, :]
-            #               output_neuron = prototypes.reshape((28, 28), order='F')
-            #               axes_neurs[ii, jj].clear()
-            #               axes_neurs[ii, jj].imshow(output_neuron,
-            #                                       interpolation='nearest', cmap='inferno')
+        if not (t % 300 or t == 1) and show:
+            for ii in range(yl):
+                for jj in range(5):
+                    if 5*ii+jj < digits:
+                        prototypes = W[5*ii+jj, :]
+                        output_neuron = prototypes.reshape((28, 28), order='F')
+                        axes_neurs[ii, jj].clear()
+                        axes_neurs[ii, jj].imshow(output_neuron,
+                                                interpolation='nearest', cmap='inferno')
 
-            #   # plot stats
-            #   axes_stats[0].clear()
-            #   axes_stats[0].bar(np.arange(1, digits+1), np.reshape(h, digits),
-            #                   align='center', color='#57106e')
-            #   axes_stats[0].set_xticks(np.arange(1, digits+1))
-            #   axes_stats[0].relim()
-            #   axes_stats[0].autoscale_view(True, True, True)
+            # plot stats
+            axes_stats[0].clear()
+            axes_stats[0].bar(np.arange(1, digits+1), np.reshape(h, digits),
+                            align='center', color='#57106e')
+            axes_stats[0].set_xticks(np.arange(1, digits+1))
+            axes_stats[0].relim()
+            axes_stats[0].autoscale_view(True, True, True)
 
-            #   axes_stats[1].clear()
-            #   axes_stats[1].imshow(x.reshape((28, 28), order='F'),
-            #                       interpolation='nearest', cmap='inferno')
-            #   axes_stats[1].get_xaxis().set_ticks([])
-            #   axes_stats[1].get_yaxis().set_ticks([])
+            axes_stats[1].clear()
+            axes_stats[1].imshow(x.reshape((28, 28), order='F'),
+                                interpolation='nearest', cmap='inferno')
+            axes_stats[1].get_xaxis().set_ticks([])
+            axes_stats[1].get_yaxis().set_ticks([])
 
-            #   axes_stats[2].clear()
-            #   axes_stats[2].imshow(W[k, :].reshape((28, 28), order='F'),
-            #                       interpolation='nearest', cmap='inferno')
-            #   axes_stats[2].get_xaxis().set_ticks([])
-            #   axes_stats[2].get_yaxis().set_ticks([])
+            axes_stats[2].clear()
+            axes_stats[2].imshow(W[k, :].reshape((28, 28), order='F'),
+                                interpolation='nearest', cmap='inferno')
+            axes_stats[2].get_xaxis().set_ticks([])
+            axes_stats[2].get_yaxis().set_ticks([])
 
-            #   axes_stats[3].clear()
-            #   axes_stats[3].plot(wCount[0, 2:t+1], linewidth=2.0, label='rate')
-            #   axes_stats[3].set_ylim([-0.001, 0.3])
-            #   axes_stats[3].legend()
+            axes_stats[3].clear()
+            axes_stats[3].plot(wCount[0, 2:t+1], linewidth=2.0, label='rate')
+            axes_stats[3].set_ylim([-0.001, 0.3])
+            axes_stats[3].legend()
 
-            #   axes_stats[4].clear()
-            #   axes_stats[4].bar(np.arange(1, digits+1), np.reshape(counter.T, digits),
-            #                   align='center', color='#57106e')
+            axes_stats[4].clear()
+            axes_stats[4].bar(np.arange(1, digits+1), np.reshape(counter.T, digits),
+                            align='center', color='#57106e')
 
             print(dead_units)
-        
-            #   plt.show()
-            #   plt.pause(0.0001)
+            
+            plt.show()
+            plt.pause(0.0001)
 
+    # Saving the figures ----------------------------------------
     plt.waitforbuttonpress()
     fig_neurs.savefig('fig_neurs.pdf')
     fig_stats.savefig('fig_stats.pdf')
 
-    extent = axes_stats[3].get_window_extent().transformed(fig_stats.dpi_scale_trans.inverted())
-    fig_stats.savefig('average_weight_changes.pdf', bbox_inches=extent.expanded(1.1, 1.2))
+    axes_stats[3].get_window_extent().transformed(fig_stats.dpi_scale_trans.inverted())
+    fig_stats.savefig('average_weight_changes.pdf')
+    # might need to include bbox_inches=extent.expanded(1.1, 1.2)
 
     plt.clf()
     plt.cla()
     plt.close()
 
+    # Correlation matrix of the prototypes
     corr = np.corrcoef(W)
     plt.figure()
     plt.imshow(corr, interpolation="nearest", cmap="inferno")
     plt.title('Correlation Of Prototypes')
+    plt.ylabel("prototype")
+    plt.xlabel("prototype")
     plt.colorbar()
     plt.savefig('correlation_matrix.pdf')
 
